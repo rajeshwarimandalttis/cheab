fad4e0d22325524ce8da6835a18bbc2712edf353
Dipanjan Kundu <rajeshwarimandalttis@gmail.com>
2021-12-14 22:32:24 +0530

    Added home page

--- script.js
+++ script.js
@@ -90,20 +90,4 @@ $(document).ready(function(){
         }
 
     });
-
-    var parallax = -0.5;
-
-    var $bg_images = $(".main");
-    var offset_tops = [];
-    $bg_images.each(function(i, el) {
-        offset_tops.push($(el).offset().top);
-    });
-
-    $(window).scroll(function() {
-        var dy = $(this).scrollTop();
-        $bg_images.each(function(i, el) {
-            var ot = offset_tops[i];
-            $(el).css("background-position", "50% " + (dy - ot) * parallax + "px");
-        });
-    });
 });
